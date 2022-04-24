da19e37c8ce7ffdd92168df804ac48e3c1c29645
ViktoriiaKudina <50966199+ViktoriiaKudina@users.noreply.github.com>
2022-04-24 17:49:32 +0300

    test 2

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,6 +1,12 @@
 
 public class MainClass {
 
+    private int classNumber = 20;
+
+    public int getClassNumber() {
+        return classNumber;
+    }
+
     public int getLocalNumber() {
         return 14;
     }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -9,6 +9,11 @@ public class MainClassTest {
 
     @Test
     public void testGetLocalNumber() {
-        Assert.assertEquals("Local number should be 14",14,mainClass.getLocalNumber());
+        Assert.assertEquals("Local number should be 14",14, mainClass.getLocalNumber());
+    }
+
+    @Test
+    public void testGetClassNumber() {
+        Assert.assertTrue("The number should be over 45",mainClass.getClassNumber() > 45);
     }
 }
